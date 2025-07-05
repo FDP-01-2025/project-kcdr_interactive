2ae792f3ab2e771ec5cd5b6f33b879ce8c5c4bc9
RodJMA <00110125@uca.edu.sv>
2025-07-04 21:09:47 -0600

    Juego ya con menu

--- main.cpp
+++ main.cpp
@@ -6,14 +6,16 @@
 #include "src/Player.h"
 #include "src/enemy_utils.h"
 #include "src/configurationDifficulty.h"
+#include "src/Boss.h"
+#include "src/BossDraw.h"
+#include "src/GameMenu.h"
+#include "src/SaveSystem.h"
 
 using namespace std;
 
 int main()
 {
-    chooseCharacterAndSave();
-    configurationDifficulty difficulty = selectDifficult();
-    playGame();
-    
+    GameMenu::displayTitleScreen();  // Epic title screen
+    GameMenu::displayMainMenu();     // Complete main menu
     return 0;
 }
